5023886969cd5a2355aeca02f9a83f32f0d44735
Nipuna Bhanuka <nipunahettiwatta@gamil.com>
2024-01-24 10:23:07 +0530

    change author model

--- TaskAPI.Models/Author.cs
+++ TaskAPI.Models/Author.cs
@@ -9,6 +9,11 @@ namespace TaskAPI.Models
     public class Author
     {
         public int Id { get; set; }
-        public required string AuthorName { get; set; }
+        public required string AuthorName { get; set; } 
+        public string AddressNo { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+
+        public ICollection<Todo> Todos { get; set; } = new List<Todo>();
     }
 }
